59e90d3b7f3684d6774d9bba2d69d5a366d0886d
Arman226 <armanbibz@gmail.com>
2021-04-03 09:30:46 +0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Armando Bibay Jr on 4/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
